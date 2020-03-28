7abb3d58854f582ec649eec07a07f153ff237b64
Immijimmi <immijimmi1@gmail.com>
2020-03-28 16:14:33 +0000

    Simplified player (most functionality is provided in Phaser))

--- player.js
+++ player.js
@@ -1,11 +1,6 @@
 class Player {
     constructor(phaser_object, coords, hp, velocity) {
         this.phaser_object = phaser_object;
-        this.x_coord = coords[0];
-        this.y_coord = coords[1];
-        this.curr_hp = hp[0];
-        this.max_hp = hp[1];
-        this.x_vel = velocity[0];
-        this.y_vel = velocity[1];
+        this.hp = hp;
     }
 }
