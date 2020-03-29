82fe1c69d5142ffd7f6f4c2a2e622597c13666ca
Immijimmi <immijimmi1@gmail.com>
2020-03-29 02:13:29 +0100

    Powerups Ticket
    Created placeholder images for 3 types of powerup, coded a Phaser group for them and modified Player class to receive powerups

--- index.js
+++ index.js
@@ -21,10 +21,20 @@ const game = new Phaser.Game(config);
 
 function preload(){
     this.load.image('spike', 'assets/spike.png');
+
+    this.load.image('hp_powerup', 'assets/hp_powerup.png');
+    this.load.image('timer_powerup', 'assets/timer_powerup.png');
+    this.load.image('shield_powerup', 'assets/shield_powerup.png');
 }
 
 function create(){
     this.add.image(300, 450, 'spike');
+
+    powerups = this.physics.add.staticGroup();
+    
+    powerups.create(100, 100, 'hp_powerup');
+    powerups.create(100, 200, 'timer_powerup');
+    powerups.create(100, 300, 'shield_powerup');
 }
 
 function update(){
--- player.js
+++ player.js
@@ -2,5 +2,19 @@ class Player {
     constructor(phaser_object, coords, hp, velocity) {
         this.phaser_object = phaser_object;
         this.hp = hp;
+
+        this.invuln_timer = 0;
+        this.level_timer = 0;
+    }
+
+    powerup(powerup_name) {
+        switch (powerup_name, powerup_value) {
+            case 'hp_powerup':
+                this.hp[0] = Math.min(this.hp[0]+1, this.hp[1])
+            case 'timer_powerup':
+                this.level_timer -= powerup_value;
+            case 'shield_powerup':
+                this.invuln_timer += powerup_value; 
+        }
     }
 }
